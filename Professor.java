579ba734f36278dbf0b4424253e012b30197c967
aaronmanoj23 <aaronmanoj23@yahoo.com>
2025-01-15 14:40:22 -0800

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,4 +1,40 @@
+import javax.persistence.*;
 
+@Entity
+@Table(name = "professors")
 public class Professor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String department;
+
+    @OneToOne(mappedBy = "professor", cascade = CascadeType.ALL)
+    private Customer customer;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
