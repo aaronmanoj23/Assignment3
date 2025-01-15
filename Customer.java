67359520f6ffe1f88eb31c01e1ee5d7a0544a8d7
aaronmanoj23 <aaronmanoj23@yahoo.com>
2025-01-15 14:34:38 -0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,41 @@
+import javax.persistence.*;
 
+@Entity
+@Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
