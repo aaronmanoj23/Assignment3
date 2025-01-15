39cdc7ceb1fcf426165135899e35d4f7ee2b0ef8
aaronmanoj23 <aaronmanoj23@yahoo.com>
2025-01-15 14:39:06 -0800

    Update MainDeleteOrder.java

--- MainDeleteOrder.java
+++ MainDeleteOrder.java
@@ -1,4 +1,18 @@
+import org.hibernate.Session;
 
 public class MainDeleteOrder {
 
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        // Delete Order
+        Order order = session.get(Order.class, 1L); // Change ID as needed
+        if (order != null) {
+            session.remove(order);
+        }
+
+        session.getTransaction().commit();
+        session.close();
+    }
 }
